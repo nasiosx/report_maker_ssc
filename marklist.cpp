54ad8a07d0a0d56a1f7209102e88f862f4d1285d
nasiosx <blackhatosx@gmail.com>
2020-06-07 07:50:06 +0530

    Added comments and a simple login function

--- marklist.cpp
+++ marklist.cpp
@@ -1,42 +1,96 @@
+/**
+ * Developer : NASIOSX
+ * Program name : Report Maker
+ * Description : A simple program in c++ for making students report card
+ * 
+ * #pragma Optimise , #pragma taget: Code Optimisation 
+ * 			See : https://www.geeksforgeeks.org/speed-up-naive-algorithms-in-competitive-coding-in-c-cpp/
+ * 
+ * <fstream> FILE I/O Operations , Eg: for storing input data to a text file
+ * 			See : https://www.geeksforgeeks.org/file-handling-c-classes/
+ * 
+ * <ctime> Time header file , Retrieves localtime
+ * 			See : https://www.geeksforgeeks.org/c-program-print-current-day-date-time/
+ * 
+ * Functions Overview :-
+ * 
+ * 	bool check(string arr)
+ * 		data type bool , returns bool value (1/0) 
+ * 		Argument type string, Accepts std::string data type variables for processing
+ * 		Description : Check a string for the presence of numbers in it , returns true if found
+ *			std::string See : https://www.geeksforgeeks.org/stdstring-class-in-c/ 	
+ *	
+ *  void Execute()
+ * 		data type void , returns no value
+ * 		Argument type void , doesn't accept any arguments
+ * 		Description : Core function of the program , Ask user for inputs and store them into a file
+ * 
+ * 	void history(int n, char** arr)
+ * 		data type void , returns no value
+ * 		Argument type int , char** : Accepts a integer variable/value and char** which is pointer to char*
+ * 			Reference for char** // https://stackoverflow.com/questions/13353807/what-is-char-in-c
+ * 		Description : Accepts data from core function and store it to a log / history file 		
+ * 
+ * 	void manage(int n)
+ * 		data type void , returns no value
+ * 		Argument type int , Accepts integer variable/value
+ * 		Description : Function to handle log / history file , Deleting log file and Reading log file are supported
+ * 
+ * 	void banner()
+ * 		data type void , returns no value
+ * 		Argument type void , doesn't accept any arguments
+ * 		Description : Function to display ASCII ART 
+ * 					Useful link for ascii arts : http://patorjk.com/software/taag/
+ * 
+ * 	void login(string uname, string pass)
+ * 		data type void , returns no value
+ * 		Argument type string , Accepts string value
+ * 		Description : Function to validate username and password
+ *  
+ * 	int main()
+ * 		data type int , returns integer value , return 0 on a successfull execution
+ * 		Argument type void , doesn't accept any arguments
+ * 		Description : Starting point of the program
+ * */ 
+
+
+#pragma Optimise("Ofast")
+#pragma target("avx,avx2,fma")
 #include <iostream>
 #include <cstdio>
 #include <cstdlib>
-#include <cstring>
-#include <fstream>
-#include <ctime>
+#include <cstring> 
+#include <fstream> 
+#include <ctime> 
 using namespace std;
 
-bool check(string arr);
-void Execute();
+bool check(string arr); 
+void Execute(); 
 void history(int n, char** arr);
 void manage(int n);
 void banner();
+void login(string uname, string pass);
 
-struct students_details{
+struct students_details{ // Structure Declaration
 		string name;
 		int marks[6];
 		int roll_num;
 		int total = 0;
 		float average;
 		float percentage;
-} ssc[63];
+};
+struct students_details ssc[64]; // Variable Declaration of structure , Here Array of Structures
+
+// See : https://www.geeksforgeeks.org/structures-in-cpp/
 
 int main()
 {
-	bool lnx;
-	#ifdef __linux__
-		lnx = true;
-	#elif __unix
-		lnx = true;
-	#endif
-
-	if(!lnx)
-	{
-		cout << "\n Sorry, This program Only Supports Linux \n";
-		exit(0);
-	}
+	// Change Username and Password as you wish
+	const string uname = "admin"; // constant Username
+	const string pass = "admin"; // constant Password
+	login(uname, pass);
 	int op;
-	banner();
+	banner(); // Call banner() , Display ASCII Art
 	cout << "\n\t\t\t\t 1. Create a New Report " << endl;
 	cout << "\t\t\t\t 2. Check the Report log "  << endl;
 	cout << "\t\t\t\t 3. Delete the Report log " << endl;
@@ -47,11 +101,11 @@ int main()
 	switch(op)
 	{
 		case 1:
-			Execute();
+			Execute(); // Call Execute() , program execution jumps into Execute() function
 			break;
 		case 2:
 		case 3:
-			manage(op);
+			manage(op); // Call manage() with integer arguement op
 			break;
 		case 4:
 			cout << "\n\t\t\t\t Created by Nasi Nashid " << endl;
@@ -59,8 +113,8 @@ int main()
 			cout << "\t\t\t\t History logs are stored in .report_history file and can be accessed using Option 3 in main menu " << endl;
 			cout << "\t\t\t\t Thank you for using my program " << endl;
 		default:
-			cout << "\n\t\t\t\t Exiting Program , Thank you " << std::endl;
-			exit(0);
+			cout << "\n\t\t\t\t Exiting Program , Thank you " << endl;
+			exit(0); // Program control jumps to return 0
 	}
 	return 0;
 }
@@ -72,9 +126,7 @@ cout << "\t\t\t                      __________________________ "  << endl;
 cout << "\t\t\t _____________________    __|__ \\_  __ \\_|__ \\_  __ \\ " << endl;
 cout << "\t\t\t __  ___/_  ___/  ___/    ____/ /  / / /___/ /  / / / " << endl;
 cout << "\t\t\t _(__  )_(__  )/ /__      _  __// /_/ /_  __// /_/ /  " << endl;
-cout << "\t\t\t /____/ /____/ \\___/      /____/\\____/ /____/\\____/  " << endl;
-                                                    
-                                                                 
+cout << "\t\t\t /____/ /____/ \\___/      /____/\\____/ /____/\\____/  " << endl;                            
 }
 
 void Execute()
@@ -87,18 +139,20 @@ void Execute()
 						" Maths : ",
 						" Computer Science : ",
 						" English : ",
-						" First Language : "};
+						" First Language : "}; 
 	
+	// Character Pointer to hold subjects names
+
 	cout << "\n\n\t\t\t\t STUDENTS MARKLIST SSC 2020 \t\t\n";
 	cout << "\n\t\t Enter the number of reports : ";
 	cin >> numberOfreports;
-	for(int i=0; i < numberOfreports; i++)
+	for(int i=0; i < numberOfreports; i++) // Loop till i is not equal to numberOfreports
 	{
 		cout << "\n\t\t REPORT CARD " << i+1 << " :- " << std::endl;
 		cout << "\t\t ----------------------------- \n";
 		do{
 			cout << "\t\t Enter the name of the student : ";
-			cin >> ssc[i].name;
+			cin >> ssc[i].name; // Accessing members of structure students_details
 			if(check(ssc[i].name))
 				cout << "\n\t\t Name cannot contain numbers , Please enter the name again in  correct format " << std::endl;
 		}while(check(ssc[i].name));
@@ -122,13 +176,14 @@ void Execute()
 		{
 			if(j==0)
 			{
-				fstream tFile;
-				tFile.open("report.txt", ios::out);
-				tFile << "\n\t\t\t STUDENTS MARKLIST SSC 2020 \n\n";
-				tFile.close();
+				fstream tFile;  // Declaring variable to hold data for storing to or reading from a file
+				tFile.open("report.txt", ios::out);  // Open "report.txt" in Output Mode , Overwrites the file on each execution 
+				tFile << "\n\t\t\t STUDENTS MARKLIST SSC 2020 \n\n"; // Datas inserted to tFile variable is stored into report.tx
+				tFile.close(); // Store all content in tFile variable to "report.txt" and close the file
 			}
 			fstream outFile;
-			outFile.open("report.txt", ios::out | ios::app);
+			outFile.open("report.txt", ios::out | ios::app); /** Open file in Output Mode and Append Mode , Append Mode stores contents of variable
+															   * at the end of the file , Thus preventing Overwriting on each execution **/
 			outFile << "\n\n REPORT CARD " << j+1 << "\n";
 			outFile << " ------------------------ \n\n";
 			outFile << " Name : " << ssc[j].name << endl;
@@ -140,7 +195,7 @@ void Execute()
 		
 		}
 		cout << "\n\t\t Done , Check report.txt in the current Directory !" << endl;
-		history(numberOfreports, subjects);
+		history(numberOfreports, subjects); // Calls history() with integer and char*
 	}
 	else{
 		cout << "\n\t\t Exiting " << endl;
@@ -149,28 +204,28 @@ void Execute()
 
 bool check(string arr)
 {
-	for(int i=0; arr[i]!='\0'; i++)
+	for(int i=0; arr[i]!='\0'; i++) // Loop until the end of string
 	{
-		if(arr[i] >= '0' && arr[i] <= '9')
+		if(arr[i] >= '0' && arr[i] <= '9') // Check each character for between 0 and 9
 		{
-			return true;
+			return true; // if a character is a number , return true and exit the function
 		}
 	}
-	return false;
+	return false; 
 }
 
 void history(int n, char** arr)
 {
-	time_t tt;
-	struct tm * ti;
-	time(&tt);
-	ti = localtime(&tt);
+	time_t tt; // Declaring variable to store time
+	struct tm * ti; // Declaring pointer variable for structure tm
+	time(&tt); 
+	ti = localtime(&tt); 
 	fstream tempFile;
 	tempFile.open(".report_history", ios::out | ios::app);
 	for(int i=0; i < n ; i++)
 	{
 		if(i==0)
-			tempFile << "\n DATE : " << asctime(ti) << endl;
+			tempFile << "\n DATE : " << asctime(ti) << endl; // Store time and date in human-readable form 
 		tempFile << " REPORT CARD " << i+1 << endl;
 		tempFile << " ----------------------- " << endl;
 		tempFile << " Name : " << ssc[i].name << endl;
@@ -190,22 +245,56 @@ void manage(int n)
 {
 	if(n == 2)
 	{
-		string line;
-		ifstream inFile;
-		inFile.open(".report_history");
-		if(!inFile)
+		string line; // String variable to store contents of a file
+		fstream inFile; // Declaring variable to hold contents of file
+		inFile.open(".report_history", ios::in);  // Open file in Input Mode / Read Mode
+		if(!inFile) // If variable return 0 , Error in opening file
 			cout << "\t\tUnable to open file , Maybe you are running this program for the first time , Thank You \n" << endl;
-		while(inFile)
+		while(inFile) // Loop until the end of file
 		{
-			getline(inFile, line);
-			cout << line << endl;
+			getline(inFile, line); // retrieve contents from file variable to string variable
+			cout << line << endl; // Display contents of string variable , That is , lines in the file
 		}
-		inFile.close();
+		inFile.close(); // Close the file after Reading
 		
 	}
 	else if(n == 3)
 	{
-		system("rm .report_history > /dev/null");
-		cout << "\n Logs Successfully Deleted " << endl;
+		if(remove(".report_history")==0) // remove("file") , removes file named "file" in the current directory , returns 0 on successful remove 
+			cout << "\n Logs Successfully Deleted " << endl;
+		else
+			cout << "\n Error while deleting file " << endl;;
+		
+	}
+}
+
+void login(string uname, string pass)
+{
+	string userName, passWord;
+	cout << "\n Note : Default Username and Password = admin \n\n";
+	cout << "\n Enter the username : ";
+	cin >> userName;
+	cout << "\n Enter the password : ";
+	cin >> passWord;
+	if(uname.compare(userName) == 0 && pass.compare(passWord) == 0)
+	{
+		cout << "\n Login Success " << endl;
+		getchar();
+
+		// Macros to Identify Operating System
+		// See : https://sourceforge.net/p/predef/wiki/OperatingSystems/
+		// Also : https://stackoverflow.com/questions/15580179/how-do-i-find-the-name-of-an-operating-system
+		#ifdef _WIN32
+			system("cls");
+		#elif _WIN64
+			system("cls");
+		#elif __linux__
+			system("clear");
+		#endif
+	}
+	else
+	{
+		cout << "\n Invalid Username or Password , Please try again \n";
+		exit(0);
 	}
 }
